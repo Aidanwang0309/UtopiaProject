a63ca196bedb1918647ad7ac3c37fb5997a086f9
aidanwang0309 <aidanwang9303@gmail.com>
2017-12-16 05:23:18 -0500

    three posters update/ Homepageupdate

--- js/main.js
+++ js/main.js
@@ -2,30 +2,64 @@ $(function () { // wait for document ready
     // init
     var controller = new ScrollMagic.Controller();
 
+
+    var movingleftHand = new TimelineMax()
+        .fromTo(".right-hand", 1, {x: "200%", y:"-10%"}, {x: "100%", y:"20%", ease: Linear.easeNone})
+
+    new ScrollMagic.Scene({
+        triggerElement:"#hands-trigger",
+        offset:350,
+        duration:450
+
+    })
+        .setPin("#hands-trigger")
+        .setTween(movingleftHand)
+        .addTo(controller);
+
+
+
+    var movingleftHand = new TimelineMax()
+        .fromTo(".left-hand", 1, {x: "-100%", y:"-50%"}, {x: "20%", y:"0%", ease: Linear.easeNone})
+
+    new ScrollMagic.Scene({
+        triggerElement:"#hands-trigger",
+        offset:350,
+        duration:500
+
+    })
+        .setPin("#hands-trigger")
+        .setTween(movingleftHand)
+        .addTo(controller);
+
+    //
+
+
+    new ScrollMagic.Scene({
+        triggerElement:"#hands-trigger",
+        offset:1000,
+    })
+        .setPin("#hands-trigger")
+        .setTween(".grey", 5, {filter: "grayscale(0%)"})
+        .addTo(controller);
+
+
     // define movement of panels
     var wipeAnimation = new TimelineMax()
-    // animate to second panel
-        .to("#slideContainer", 0.5, {z: -150})		// move back in 3D space
-        .to("#slideContainer", 1,   {x: "-25%"})	// move in to first panel
-        .to("#slideContainer", 0.5, {z: 0})				// move back to origin in 3D space
-        // animate to third panel
-        .to("#slideContainer", 0.5, {z: -150, delay: 1})
-        .to("#slideContainer", 1,   {x: "-50%"})
-        .to("#slideContainer", 0.5, {z: 0})
-        // animate to forth panel
-        .to("#slideContainer", 0.5, {z: -150, delay: 1})
-        .to("#slideContainer", 1,   {x: "-75%"})
-        .to("#slideContainer", 0.5, {z: 0});
+        .fromTo("section.panel.white", 1, {x: "-100%"}, {x: "0%", ease: Linear.easeNone})  // in from left
+        .fromTo("section.panel.green",    1, {x:  "100%"}, {x: "0%", ease: Linear.easeNone})  // in from right
+        .fromTo("section.panel.bordeaux", 1, {y: "-100%"}, {y: "0%", ease: Linear.easeNone}) // in from top
+        // .fromTo(".left-head",1,{y: "0%"}, {y: "0%", ease: Linear.easeNone})
+        .fromTo(".right-head",1,{y: "-50%"}, {y: "0%", ease: Linear.easeNone});
 
     // create scene to pin and link animation
     new ScrollMagic.Scene({
         offset:-60,
         triggerElement: "#pinContainer",
         triggerHook: "onLeave",
-        duration: "500%"
+        duration: "300%"
     })
         .setPin("#pinContainer")
         .setTween(wipeAnimation)
-        // .addIndicators() // add indicators (requires plugin)
         .addTo(controller);
+
 });
